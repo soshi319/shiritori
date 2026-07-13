8c72e1910c858fd66ab2e13eb58da9b54bf3cf94
soshi319 <chocobo.hoshi@gmail.com>
2026-07-13 23:13:49 +0900

    vite.configの変更

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
+
+// __dirname の代わりを定義
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      shared: path.resolve(__dirname, "../shared"),
+      // path.resolve を使わずに、直接パスを指定する形にします
+      shared: resolve(__dirname, "../shared"),
     },
   },
 });
